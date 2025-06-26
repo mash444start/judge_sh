bfba60ac77008648a0da59aa4b087623ba694a07
mash444start <mashrafetonmoy841@gmail.com>
2025-06-26 22:25:29 +0600

    Create worker.js

---
+++ api/worker.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const apiUrl = process.env.API_URL || 'http://localhost:3000';
+
+console.log("Worker started...");
+console.log("API URL is:", apiUrl);
+
+setInterval(async () => {
+  try {
+    const res = await axios.get(`${apiUrl}/languages`);
+    console.log("Available languages:", res.data);
+  } catch (err) {
+    console.error("Worker error:", err?.message || err);
+  }
+}, 10000);
